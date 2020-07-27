4f724e181ddd646341b12070f4845a3493016efb
Zylvian <33404765+Zylvian@users.noreply.github.com>
2020-07-27 16:45:09 +0200

    Update build_and_upload.sh

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-version=$(git describe --tags)
-poetry version "${version}"
 poetry build
 TWINE_USERNAME="$INPUT_PYPI_USERNAME" TWINE_PASSWORD="$INPUT_PYPI_PASSWORD" twine upload dist/* --repository-url https://pypi.org/project/wommit/
